a2db6e684b3553c0c7ad2f9fad28ff3d2b2bdd5c
Lari Mikael Orpana <lari.m.orpana@student.jyu.fi>
2019-09-11 21:42:30 +0300

    Updates

---
+++ precache-manifest.4f959ae986b1a09b3df2acee5985d91b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62f324ddfde2ba36dccdabf3217801d",
+    "url": "/infoapp/index.html"
+  },
+  {
+    "revision": "c96210172f1e2dc808f3",
+    "url": "/infoapp/static/css/main.31653456.chunk.css"
+  },
+  {
+    "revision": "02a1d7007a175eebaa47",
+    "url": "/infoapp/static/js/2.e6fc8894.chunk.js"
+  },
+  {
+    "revision": "c96210172f1e2dc808f3",
+    "url": "/infoapp/static/js/main.b48a64d8.chunk.js"
+  },
+  {
+    "revision": "f1f93b6eedae774c767a",
+    "url": "/infoapp/static/js/runtime~main.b99f76b9.js"
+  }
+]);
