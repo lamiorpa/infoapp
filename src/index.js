e6502ceda409a3da35e4b8ac88ce36b18bd93b7d
Lari Mikael Orpana <lari.m.orpana@student.jyu.fi>
2019-09-13 01:14:46 +0300

    Using hashrouter for github pages compatability

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Solution to github pages routing problem (inclusion of "basename" parameter): 
 // https://medium.com/@Dragonza/react-router-problem-with-gh-pages-c93a5e243819
+// Hashrouter fixing the refresh 404 problem
+// https://stackoverflow.com/questions/46056414/getting-404-for-links-with-create-react-app-deployed-to-github-pages
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
